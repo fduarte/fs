ac89fa5383a6b674772fd70bf90966fbec960a48
Freddy <fredduarte@gmail.com>
2016-11-04 22:31:12 +0000

    Add main layout with tb3.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="FS Assignment">
+    <meta name="author" content="Freddy Duarte">
+
+    <title>FS Assignment</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="container">
+    <div class="header clearfix">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">FS Assignment</a>
+                </div>
+
+                <div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/">View People</a></li>
+                        <li><a href="/person/create">Create Person</a></li>
+                    </ul>
+                </div>
+            </div><!-- /.container-fluid -->
+        </nav>
+    </div>
+
+    <div class="row">
+
+        {{-- Display errors --}}
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <div class="col-lg-6">
+            @yield('content')
+        </div>
+    </div>
+
+    <footer class="footer">
+        <hr />
+        <p class="text-muted pull-right">Nov 2016, <a href="mailto:fredduarte@gmail.com">Freddy Duarte</a></p>
+    </footer>
+
+</div> <!-- /container -->
+
+</body>
+</html>
